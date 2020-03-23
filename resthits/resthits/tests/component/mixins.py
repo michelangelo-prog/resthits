bdca89b89a6f39c4f4c72afe44c91deff58f5836
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2020-03-23 21:05:12 +0100

    add func to test mixins

--- resthits/resthits/tests/component/mixins.py
+++ resthits/resthits/tests/component/mixins.py
@@ -40,6 +40,10 @@ class ArtistMixin(DbMixin):
         uri = "/api/v1/hits"
         return self.client.post(uri, **kwargs)
 
+    def put_update_hit(self, title_url, **kwargs):
+        uri = "/api/v1/hits/{}".format(title_url)
+        return self.client.put(uri, **kwargs)
+
     def _get_all_artists_from_db(self):
         return Artist.query.all()
 
